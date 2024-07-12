dfa741124c10ef7b1364d5c57c3a70bcbefc9c96
Алина Вдовиченко <alinavdovichenko.ru@gmail.com>
2024-07-12 13:41:24 +0500

    2.1.25. Задокументирует REST API в формате Open API.

--- project/apps/account/src/main.ts
+++ project/apps/account/src/main.ts
@@ -1,14 +1,24 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
-async function bootstrap() {
-  const GLOBAL_PREFIX = 'api';
-  const DEFAULT_PORT = 3000;
-  const app = await NestFactory.create(AppModule);
+const DEFAULT_PORT = 3000;
+const GLOBAL_PREFIX = 'api';
 
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(GLOBAL_PREFIX);
+
+  const config = new DocumentBuilder()
+    .setTitle('Account Application')
+    .setDescription('The Readme site the account application description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT || DEFAULT_PORT;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${GLOBAL_PREFIX}`);

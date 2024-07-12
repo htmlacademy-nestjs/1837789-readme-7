9c322b8ae0be5f6f77a55cd18a11c1829f6b26b2
Алина Вдовиченко <alinavdovichenko.ru@gmail.com>
2024-07-12 11:38:57 +0500

    2.1.16. Зарегистрирует BlogUserRepository и BlogUserFactory в качеcтве провайдера.

--- project/libs/account/blog-user/src/blog-user-module/blog-user.module.ts
+++ project/libs/account/blog-user/src/blog-user-module/blog-user.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { BlogUserController } from './blog-user.controller';
-import { BlogUserService } from './blog-user.service';
+import { BlogUserRepository } from './blog-user.repository';
+import { BlogUserFactory } from './blog-user.factory';
 
 @Module({
-  controllers: [BlogUserController],
-  providers: [BlogUserService]
+  providers: [BlogUserRepository, BlogUserFactory],
+  exports: [BlogUserRepository],
 })
 
 export class BlogUserModule {}

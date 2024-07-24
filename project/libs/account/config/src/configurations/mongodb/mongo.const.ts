0ed852c15efe3f8b52507838b18fadfdce94d69b
Алина Вдовиченко <alinavdovichenko.ru@gmail.com>
2024-07-24 11:03:57 +0500

    4.1.1. Подготовит PostgreSQL.

--- project/libs/account/config/src/configurations/mongodb/mongo.const.ts
+++ project/libs/account/config/src/configurations/mongodb/mongo.const.ts
@@ -1,3 +1,5 @@
-export const MIN_PORT = 0;
-export const MAX_PORT = 65535;
-export const DEFAULT_MONGO_PORT = 27017;
+export enum MongoPort {
+  MinPort = 0,
+  MaxPort = 65535,
+  DefaultMongoPort = 27017
+}
--- project/libs/shared/helpers/src/lib/common.ts
+++ project/libs/shared/helpers/src/lib/common.ts
@@ -27,5 +27,5 @@ export function fillDto<T, V extends PlainObject>(
 
 export function getMongoConnectionString({username, password, host, port, databaseName, authDatabase}): string {
   console.info(`mongodb://${username}:${password}@${host}:${port}/${databaseName}?authSource=${authDatabase}`);
-  return `mongodb://${username}:${password}@${host}:${port}/${databaseName}?authSource=${authDatabase}`;
+  return `mongodb://${host}:${port}/${databaseName}?authSource=${authDatabase}`;
 }

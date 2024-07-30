cd36fb236c456f180ffdcfe2e7759ad0f2489e09
Алина Вдовиченко <alinavdovichenko.ru@gmail.com>
2024-07-30 17:16:08 +0500

    5.1.5. Активирует трансформацию для ValidationPipe.

--- project/libs/account/authentication/src/authentication-module/authentication.controller.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpStatus, Param, Post, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Post } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthenticationService } from './authentication.service';
 import { CreateUserDto } from '../dto/create-user.dto';
@@ -65,7 +65,7 @@ export class AuthenticationController {
   }
 
   @Get('/demo/:id')
-  public async demoPipe(@Param('id', ParseIntPipe) id: number) {
+  public async demoPipe(@Param('id') id: number) {
     console.log(typeof id);
   }
 }

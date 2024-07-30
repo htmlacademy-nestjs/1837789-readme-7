9a54450b85ca5aaa477abde9b6bc5f6f2978ed5e
Алина Вдовиченко <alinavdovichenko.ru@gmail.com>
2024-07-30 13:17:15 +0500

    5.1.1. Добавит базовую валидацию для сервиса account.

--- project/libs/account/authentication/src/dto/create-user.dto.ts
+++ project/libs/account/authentication/src/dto/create-user.dto.ts
@@ -1,33 +1,42 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
+import { AuthenticationValidateMessage } from '../authentication-module/authentication.constant';
 
 export class CreateUserDto {
   @ApiProperty({
     description: 'User unique address',
     example: 'user@user.ru'
   })
+  @IsEmail({}, { message: AuthenticationValidateMessage.EmailNotValid })
   public email: string;
 
   @ApiProperty({
     description: 'User first name',
     example: 'Keks',
   })
+  @IsString()
   public firstname: string;
 
   @ApiProperty({
     description: 'User last name',
     example: 'Kotovsky'
   })
+  @IsString()
   public lastname: string;
 
   @ApiProperty({
     description: 'Valid URL',
     example: 'https://15.design.htmlacademy.pro/static/avatar/4.jpg'
   })
-  public avatarUrl: string;
+  @IsOptional()
+  @IsString()
+  public avatarUrl?: string;
 
   @ApiProperty({
     description: 'User password',
     example: '1234567'
   })
+  @IsString()
   public password: string;
 }

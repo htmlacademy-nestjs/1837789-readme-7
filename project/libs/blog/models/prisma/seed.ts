251327faccaf144847496e3dd51826d88dc70c23
Алина Вдовиченко <alinavdovichenko.ru@gmail.com>
2024-08-05 13:40:19 +0500

    6.1.1. Добавит поля для сидирования.

--- project/libs/blog/models/prisma/seed.ts
+++ project/libs/blog/models/prisma/seed.ts
@@ -1,9 +1,20 @@
 import { PrismaClient } from '@prisma/client';
 
-enum PostUuid {
-  First = '6d308040-06a2-4162-bea6-2398e9976540',
-  Second = '6g308045-98a2-4162-iea6-2338e9906540',
-}
+const mockUsersId = [
+  '65a3f63cc013e4c03afc6a9d',
+  '65a3f64beb0cae3804f7d9ef',
+  '65a3f657fb04b4c2846e9094',
+  '65a3f65f2c1c3209873906fb',
+  '65a3f66ac7d55ebb0a1b9b65'
+];
+
+const mockPostId = [
+  '6d308040-06a2-4162-bea6-2398e9976540',
+  '6d308040-06a2-4162-bea6-2398e9976541',
+  '6d308040-06a2-4162-bea6-2398e9976542',
+  '6d308040-06a2-4162-bea6-2398e9976543',
+  '6d308040-06a2-4162-bea6-2398e9976544'
+];
 
 enum UserId {
   First = '658170cbb974e9f5b946pcf4',
@@ -23,48 +34,69 @@ enum PostStatus {
   Draft = 'Draft'
 }
 
-function getPosts() {
-  return [
-    {
-      id: PostUuid.First,
-      type: PostType.Text,
-      status: PostStatus.Draft,
-      userId: UserId.First,
-      title: 'First Post',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      tags: ['#aggd', '#oooo'],
-      isRepost: false,
-    },
-    {
-      id: PostUuid.Second,
-      type: PostType.Quotation,
+function randomInt (a = 1, b = 0) {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  return Math.floor(lower + Math.random() * (upper - lower + 1))
+};
+
+function getRandomElement <T>(list: T[]): T {
+  const randomindex = randomInt(0, list.length - 1);
+  return list[randomindex];
+};
+
+const mockComments = [
+  {
+    message: 'Первый комментарий',
+    userId: getRandomElement(mockUsersId),
+  },
+  {
+    message: 'Еще один новый комментарий',
+    userId: getRandomElement(mockUsersId),
+  },
+  {
+    message: 'Не придумал лучший комментарий, поэтому оставлю просто этот текст',
+    userId: getRandomElement(mockUsersId),
+  },
+  {
+    message: 'Здесь могла быть ваша реклама',
+    userId: getRandomElement(mockUsersId)
+  },
+  {
+    message: 'Лучший пост',
+    userId: getRandomElement(mockUsersId)
+  },
+];
+
+const mockPosts = mockPostId.map((id) => {
+  const type = getRandomElement(Object.values(PostType));
+  const randomNumber = randomInt(0, mockComments.length);
+  const userId = getRandomElement(mockUsersId);
+  return {
+      id: id,
+      type,
       status: PostStatus.Published,
-      userId: UserId.Second,
+      userId: userId,
       title: 'First Post',
       createdAt: new Date(),
       updatedAt: new Date(),
-      tags: ['#aggd', '#oooo'],
       isRepost: false,
+      tags: ['#aggd', '#oooo'],
       likes: [ UserId.First , UserId.Second],
-      comments: [
-        {
-          message: 'Это действительно отличная книга!',
-          userId: UserId.First,
-        },
-        {
-          message: 'Надо будет обязательно перечитать. Слишком много информации.',
-          userId: UserId.Second,
-        }
-      ],
-    }
-  ]
-}
+      comments: mockComments.slice(0, randomNumber),
 
-
+      name: (type === PostType.Video || type === PostType.Text) ? 'Post' : undefined,
+      urlVideo: type === PostType.Video ? 'http://sample.edu/hobbies.html' : undefined,
+      annoncement: type === PostType.Text ? 'Another text' : undefined,
+      text: type === PostType.Text ? 'Simple text' : undefined,
+      authorQuotation: type === PostType.Quotation ? 'Kbpf' : undefined,
+      textQuotation: type === PostType.Quotation ? 'Quote text example' : undefined,
+      photo: type === PostType.Photo ? 'https://pics4.city-data.com/cpicc/cfiles34653.jpg' : undefined,
+      urlLink: type === PostType.Link ? 'http://www.sample.com/?education=orange&reaction=detail' : undefined,
+      description: type === PostType.Link ? 'Another link' : undefined,
+  }
+})
 async function seedDb(prismaClient: PrismaClient) {
-  const mockPosts = getPosts();
-
   for (const post of mockPosts) {
     await prismaClient.post.upsert({
       where: { id: post.id },
@@ -83,6 +115,15 @@ async function seedDb(prismaClient: PrismaClient) {
         comments: post.comments ? {
           create: post.comments
         } : undefined,
+        name: post.name,
+        urlVideo: post.urlVideo,
+        annoncement: post.annoncement,
+        text: post.text,
+        authorQuotation: post.authorQuotation,
+        textQuotation: post.textQuotation,
+        photo: post.photo,
+        urlLink: post.urlLink,
+        description: post.description,
       }
     })
   }

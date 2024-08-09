ae07d8211a9454603f2877a80347009ae60c4cd0
Алина Вдовиченко <alinavdovichenko.ru@gmail.com>
2024-08-09 16:20:45 +0500

    6.1.29. Добавит модуль mail в notify.

--- project/libs/notify/email-subscriber/src/email-subscriber.module.ts
+++ project/libs/notify/email-subscriber/src/email-subscriber.module.ts
@@ -4,25 +4,30 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 
 import { getRabbitMQOptions } from '@project/helpers';
-import { EmailSubscriberModel, EmailSubscriberSchema } from './email-subscriber.model';
+import {
+  EmailSubscriberModel,
+  EmailSubscriberSchema,
+} from './email-subscriber.model';
 import { EmailSubscriberService } from './email-subscriber.service';
 import { EmailSubscriberRepository } from './email-subscriber.repository';
 import { EmailSubscriberFactory } from './email-subscriber.factory';
+import { MailModule } from './mail-module/mail.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: EmailSubscriberModel.name, schema: EmailSubscriberSchema }
+      { name: EmailSubscriberModel.name, schema: EmailSubscriberSchema },
     ]),
     RabbitMQModule.forRootAsync(
       RabbitMQModule,
       getRabbitMQOptions('application.rabbit')
     ),
+    MailModule,
   ],
   providers: [
     EmailSubscriberService,
     EmailSubscriberRepository,
     EmailSubscriberFactory,
-  ]
+  ],
 })
 export class EmailSubscriberModule {}

1eabf3b0afb80571da2f5d31d76e898cebdcc14d
Алина Вдовиченко <alinavdovichenko.ru@gmail.com>
2024-08-09 15:21:22 +0500

    6.1.26. Добавит модуль notify в сервис accounts.

--- project/libs/account/authentication/src/authentication-module/authentication.module.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.module.ts
@@ -3,7 +3,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { BlogUserModule } from '@project/blog-user';
 import { getJwtOptions } from '@project/account-config';
-
+import { NotifyModule } from '@project/account-notify';
 import { JwtAccessStrategy } from '../strategies/jwt-access.strategy';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
@@ -14,7 +14,8 @@ import { AuthenticationService } from './authentication.service';
     JwtModule.registerAsync({
       inject: [ConfigService],
       useFactory: getJwtOptions,
-    })
+    }),
+    NotifyModule
   ],
   controllers: [AuthenticationController],
   providers: [
